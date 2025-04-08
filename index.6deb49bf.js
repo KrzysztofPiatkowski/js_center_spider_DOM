ad75685888edd31fe108349da850d4ae706d42ed
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-04-08 15:07:56 +0200

    make build

---
+++ index.6deb49bf.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=t.offsetWidth,s=t.offsetHeigth,f=e.offsetWidth,l=e.offsetHeigth;e.style.position="absolute",e.style.top="".concat((s-l)/2,"px"),e.style.left="".concat((o-f)/2,"px");
+//# sourceMappingURL=index.6deb49bf.js.map
